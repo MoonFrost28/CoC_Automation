85e785868813543c5a74a61ea9678e687bcaccf5
MoonFrost28 <50865142+MoonFrost28@users.noreply.github.com>
2023-10-14 14:06:08 +0200

    cleaning

--- source/exeCreator.py
+++ source/exeCreator.py
@@ -3,6 +3,7 @@ import PyInstaller.__main__
 PyInstaller.__main__.run([
     'Launch_IHM.py',
     '--noconsole',
-    '--add-data=images;images',
-    '--icon=Images/CoC_icon.ico'
+    '--add-data=Images;images',
+    '--icon=Images/CoC_icon.ico',
+    '--clean'
 ])
